e73d872fb8b26fd00abcae8b823b84670f2ce540
lacra <lacraddock@gmail.com>
2024-11-21 13:55:06 +0200

    Initial commit: Ikiguide application with current session management Initial Frontend Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import IntroductionPage from './components/IntroductionPage';
+import QuestionPage from './components/QuestionPage';
+import ResultsPage from './components/ResultsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<IntroductionPage />} />
+          <Route path="/question/:id/:sessionId" element={<QuestionPage />} />
+          <Route path="/results/:sessionId" element={<ResultsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
